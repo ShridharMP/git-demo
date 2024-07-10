86a5865dfaac879adae8588ee12439eda47215d4
ShridharMP <mpshridhar2016@gmail.com>
2024-07-10 19:46:34 +0530

    main merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class TestMain {
+
 public static void main (String[] args)
       {
 	System.out.println("Hello Friends");
       }
 
-<<<<<<< Updated upstream
 public static double addition(double a,double b)
 	{
  		return a+b;
@@ -12,13 +12,13 @@ public static double addition(double a,double b)
 
 public static double substraction(double a,double b)
         {
-		if(a>b){
-                return a-b;
-		}
-		else {
-		return b-a;
-		}
-=======
+	  if(a>b){
+              return a-b;
+	    }
+	  else {
+	      return b-a;
+	   }
+	  
 public static double multipication(double a,double b)
 	{
 	   return a * b;
@@ -26,7 +26,5 @@ public static double multipication(double a,double b)
 
 public static double division(double a,double b){
            return a / b;
->>>>>>> Stashed changes
         }
-
 }
