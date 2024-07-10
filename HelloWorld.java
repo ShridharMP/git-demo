bd42f6b4d40f20da8b75fc05ec23f2abb3da145c
ShridharMP <mpshridhar2016@gmail.com>
2024-07-10 19:09:36 +0530

    demonostrate merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,22 @@
 public class TestMain {
 public static void main (String[] args)
-      {
+     {
 	System.out.println("Hello Friends");
-      }
+     }
+
+public static double addition(double a,double b)
+	{
+ 		return a+b;
+	}	
+
+public static double substraction(double a,double b)
+        {
+		if(a>b){
+                return a-b;
+		}
+		else {
+		return b-a;
+		}
+        }
+
 }
