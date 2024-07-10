b9f707987f8a06566f61fd565730abfdee5395a1
ShridharMP <mpshridhar2016@gmail.com>
2024-07-10 18:31:10 +0530

    branch1 changes commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
-class TestMain{
+public class TestMain {
 public static void main (String[] args)
-	{
-		System.out.println("Hello Friends");
-	}
+      {
+	System.out.println("Hello Friends");
+      }
 }
