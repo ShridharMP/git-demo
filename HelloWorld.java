88293a77cc3ccb7af08f8deba59b68e090a46eae
ShridharMP <mpshridhar2016@gmail.com>
2024-07-10 19:44:20 +0530

    main merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public static void main (String[] args)
 	System.out.println("Hello Friends");
       }
 
+<<<<<<< Updated upstream
 public static double addition(double a,double b)
 	{
  		return a+b;
@@ -17,6 +18,15 @@ public static double substraction(double a,double b)
 		else {
 		return b-a;
 		}
+=======
+public static double multipication(double a,double b)
+	{
+	   return a * b;
+	}
+
+public static double division(double a,double b){
+           return a / b;
+>>>>>>> Stashed changes
         }
 
 }
